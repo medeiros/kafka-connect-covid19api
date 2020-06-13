018678b6cb085e361802654fcd7d14c510c8c845
Daniel Medeiros <daniel@arneam.com>
2020-06-13 14:42:15 -0300

    Remove unnecessary comment

--- src/test/java/com/arneam/Covid19SourceTaskIT.java
+++ src/test/java/com/arneam/Covid19SourceTaskIT.java
@@ -9,16 +9,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.kafka.connect.source.SourceRecord;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/**
- * This test can be used for integration testing with the system you are integrating with. For example
- * take a look at https://github.com/jcustenborder/docker-compose-junit-extension to launch docker
- * containers for your testing.
- */
 public class Covid19SourceTaskIT {
 
   public static final int EXPECTED_TOTAL_COUNTRIES = 186;
